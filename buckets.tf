14ea9dfb8b973283e60a00104d04117482fe887b
Stephen Ball <stephen.ball@lendinvest.com>
2024-01-17 10:12:01 +0000

    Prevent bucket being deleted

--- buckets.tf
+++ buckets.tf
@@ -1,6 +1,10 @@
 resource "linode_object_storage_bucket" "terraform" {
   cluster = "fr-par-1"
   label   = "rebelinblue-terraform-state"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # resource "linode_object_storage_key" "terraform" { // FIXME: Import this
@@ -11,6 +15,10 @@ resource "linode_object_storage_bucket" "terraform" {
 #         cluster     = linode_object_storage_bucket.terraform.cluster
 #         permissions = "read_write"
 #     }
+
+#   lifecycle {
+#     prevent_destroy = true
+#   }
 # }
 
 // FIXME: This doesn't really make sense here
