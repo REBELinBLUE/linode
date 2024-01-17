1e0283b4cb47fbf3adaa4f218f7470f2f448a38b
Stephen Ball <stephen@rebelinblue.com>
2024-01-17 18:20:34 +0000

    Set terraform version

--- backend.tf
+++ backend.tf
@@ -1,10 +1,12 @@
 terraform {
   backend "s3" {
-    bucket                      = "rebelinblue-terraform-state"
-    key                         = "terraform-state.tfstate"
-    region                      = "fr-par-1"
-    endpoint                    = "fr-par-1.linodeobjects.com"
+    bucket    = "rebelinblue-terraform-state"
+    key       = "terraform-state.tfstate"
+    region    = "fr-par-1"
+    endpoints = { s3 = "https://fr-par-1.linodeobjects.com" }
+
     skip_credentials_validation = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
   }
 }
