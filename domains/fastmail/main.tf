51956804c3b3fc836b1615a4491917bdae5e8db3
Stephen Ball <stephen@rebelinblue.com>
2024-01-18 09:16:37 +0000

    Cleanup DKIM

--- domains/fastmail/main.tf
+++ domains/fastmail/main.tf
@@ -22,7 +22,24 @@ resource "linode_domain_record" "mx" {
   weight      = 0
 }
 
-resource "linode_domain_record" "domainkey" {
+# FIXME: Look at migrating to wildcard domains instead of separate domains
+# https://www.fastmail.help/hc/en-us/articles/360060591153-Manual-DNS-configuration
+# https://www.fastmail.help/hc/en-us/articles/360060591053
+# resource "linode_domain_record" "wildcard" {
+#   count = 2
+
+#   domain_id = var.root_domain_id
+
+#   name = "*"
+
+#   record_type = "MX"
+#   target      = "in${count.index + 1}-smtp.messagingengine.com"
+#   priority    = (count.index + 1) * 10
+#   ttl_sec     = 0
+#   weight      = 0
+# }
+
+resource "linode_domain_record" "dkim" {
   count = local.number_of_domains * 3 # 3 For each domain, if it increases multiple the value and change the 2s below
 
   domain_id = element(local.domain_ids, floor(count.index / 3))
@@ -30,6 +47,20 @@ resource "linode_domain_record" "domainkey" {
   record_type = "CNAME"
   name        = "fm${count.index % 3 + 1}._domainkey"
   target      = "fm${count.index % 3 + 1}.${element(local.domain_names, floor(count.index / 3))}.dkim.fmhosted.com"
+  priority    = 0
+  ttl_sec     = 0
+  weight      = 0
+}
+
+// FIXME: This might no longer be needed
+resource "linode_domain_record" "legacy_dkim" {
+  count = local.number_of_domains
+
+  domain_id = element(local.domain_ids, count.index)
+
+  record_type = "TXT"
+  name        = "mesmtp._domainkey"
+  target      = "mesmtp.${element(local.domain_names, floor(count.index / 3))}.dkim.fmhosted.com"
   priority    = 0
   ttl_sec     = 0
   weight      = 0
@@ -60,28 +91,12 @@ resource "linode_domain_record" "spf" {
   weight      = 0
 }
 
-// FIXME: This might no longer be needed
-resource "linode_domain_record" "legacy_domainkey" {
-  count = local.number_of_domains
-
-  domain_id = element(local.domain_ids, count.index)
-
-  // mesmtp.rebelinblue.com.dkim.fmhosted.com
-  record_type = "TXT"
-  name        = "mesmtp._domainkey"
-  target      = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDMfgWVBMV4wfkdrHa5TGTpQwzVGguEx6Eloni4mtfdcmaZ/zFf0cYfxJ+1D5h313+IPfk7YW7mV9QrEx3G0rnfMMNeIlqHP6FkDd6IBchVl9UeRDItRGW9Cy/g+7SxibmiELPEGI9kw1A25IoDQEFws/3hMQLhqXRbsWdQIxWnowIDAQAB"
-  port        = 80
-  priority    = 10
-  ttl_sec     = 0
-  weight      = 5
-}
-
 resource "linode_domain_record" "autodiscovery" {
   for_each = {
     "submission" = {
-      "weight"   = 1
-      "port"     = 587
-      "target"   = "smtp.fastmail.com"
+      "weight" = 1
+      "port"   = 587
+      "target" = "smtp.fastmail.com"
     },
     "imaps" = {
       "weight" = 1
