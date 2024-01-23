d544d76370175fb9572430fcde5e8d6dfd73271f
Stephen Ball <stephen@rebelinblue.com>
2024-01-23 21:32:35 +0000

    More work on bootstrap script

--- terraform/stackscripts.tf
+++ terraform/stackscripts.tf
@@ -14,15 +14,15 @@ resource "linode_stackscript" "bootstrap" {
 
     ADMIN_PASSWORD=$(randomString)
 
-    set -x
-    set -e
+    echo "export HOSTNAME=$HOSTNAME" >> /root/stackscript.txt
+    echo "export ADMIN_USERNAME=$ADMIN_USERNAME" >> /root/stackscript.txt
+    echo "export ADMIN_PASSWORD=$v" >> /root/stackscript.txt
+    echo "export ADMIN_PUBKEY=\"$ADMIN_PUBKEY\"" >> /root/stackscript.txt
 
     # Basic system setup
     get_started "" "$HOSTNAME" $(system_primary_ip)
-    #system_set_hostname "$HOSTNAME"
     system_set_timezone Europe/London
     system_configure_ntp
-    #system_add_host_entry $(system_primary_ip) "$HOSTNAME"
 
     # Install packages
     DEBIAN_FRONTEND=noninteractive apt-add-repository -y ppa:fish-shell/release-3
@@ -31,32 +31,44 @@ resource "linode_stackscript" "bootstrap" {
 
     # Secure server
     secure_server "$ADMIN_USERNAME" "$ADMIN_PASSWORD" "$ADMIN_PUBKEY"
-    add_ports 80 443 22 # FIXME: 22 shouldn't be needed here but configure_basic_firewall called by secure_server does not seem to be setting it correctly
+    add_ports 80 443 22
+    # FIXME: 22 shouldn't be needed here but configure_basic_firewall called by secure_server does not seem to be setting it correctly
+    # https://www.linode.com/community/questions/24680/bug-in-stackscript-bash-library-configure_basic_firewall-on-ubuntu-2310
     save_firewall
-    #automatic_security_updates
+    
+    # automatic_security_updates
 
-    curl -fsSL https://starship.rs/install.sh | bash -s -- -y
+    curl -fsSL https://starship.rs/install.sh | sh -s -- -y
 
-    #certbot_ssl $fqdn $soa_email_address nginx
+    # certbot_ssl $fqdn $soa_email_address nginx
 
     # Configure user profile
-    #user_add_pubkey "$ADMIN_USERNAME" "$ADMIN_PUBKEY" # FIXME: Use all SSH keys from linode account?
+    # user_add_pubkey "$ADMIN_USERNAME" "$ADMIN_PUBKEY" # FIXME: Use all SSH keys from linode account?
+
     chsh -s /usr/bin/fish "$ADMIN_USERNAME"
 
-    mkdir -p /home/$ADMIN_USERNAME/.config/fish/
+    mkdir -p /home/stephen/.config/fish/
+    touch /home/$ADMIN_USERNAME/.config/fish/config.fish
+    touch /home/$ADMIN_USERNAME/.config/starship.toml
 
     echo $ADMIN_PASSWORD >> /home/$ADMIN_USERNAME/password.txt
     echo "starship init fish | source" >> /home/$ADMIN_USERNAME/.config/fish/config.fish
-    printf "[username]\nformat = \"[\$user](\$style) on \"\n\n[hostname]\nformat = \"[linode](\$style) in \"" >>  ~/.config/starship.toml
+    printf "[username]\nformat = \"[\$user](\$style) on \"\n\n[hostname]\nformat = \"[linode](\$style) in \"" >>  /home/$ADMIN_USERNAME/.config/starship.toml
 
-    chown -R $ADMIN_USERNAME:$ADMIN_USERNAME /home/$ADMIN_USERNAME}/password.txt
-    chown -R $ADMIN_USERNAME:$ADMIN_USERNAME /home/$ADMIN_USERNAME}/.config/fish/
-    chown -R $ADMIN_USERNAME:$ADMIN_USERNAME /home/$ADMIN_USERNAME}/.config/starship.toml
+    chown -R $ADMIN_USERNAME:$ADMIN_USERNAME /home/$ADMIN_USERNAME/password.txt
+    chown -R $ADMIN_USERNAME:$ADMIN_USERNAME /home/$ADMIN_USERNAME/.config/fish/
+    chown -R $ADMIN_USERNAME:$ADMIN_USERNAME /home/$ADMIN_USERNAME/.config/starship.toml
 
     # FIXME: Expire password?
 
+    passwd -e $ADMIN_USERNAME
+
+    # Mount volume
+    mkdir /mnt/data
+    echo "/dev/sdc 	 /mnt/data 	 ext4 	 defaults,noatime,nofail 0 2" >> /etc/fstab
+
     # Cleanup
-    #stackscript_cleanup
+    stackscript_cleanup
     all_set
 
     # Longview?
